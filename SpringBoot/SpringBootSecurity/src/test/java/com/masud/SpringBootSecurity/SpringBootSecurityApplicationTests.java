329ad6e262500730d6210f9b57ef83009e46d00a
Masud Rana <masudrana.20091@gmail.com>
2024-01-04 13:36:58 +0600

    add

---
+++ SpringBoot/SpringBootSecurity/src/test/java/com/masud/SpringBootSecurity/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masud.SpringBootSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
